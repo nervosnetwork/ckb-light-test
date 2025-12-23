01d9353cc67363a6b716819c35d63fcfc66c376e
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2025-12-23 21:53:17 +0800

    Create prepare_wasm_node.sh

---
+++ prepare_wasm_node.sh
@@ -0,0 +1,12 @@
+set -x
+git submodule init
+git submodule update
+cd ckb-light-wasm-demo
+git checkout gp/support-wasm-node
+git log -1
+git submodule init
+git submodule update
+cd ckb-light-client
+git log -1
+cd ../
+bash build_node.sh
