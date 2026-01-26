fbaef83dd5e7d944cee9f936f07c61d1909f9aa9
gpBlockchain <744158715@qq.com>
2026-01-26 11:36:26 +0800

    update prepare sh

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,6 @@ mkdir tmp
 cd tmp
 git clone https://github.com/gpBlockchain/startBlockchain.git
 cd startBlockchain
-git checkout gp/support-sqlite
 cd ckbLightClient
 sh prepare.sh
 sh start.sh
--- prepare_dev.sh
+++ prepare_dev.sh
@@ -2,7 +2,6 @@ mkdir tmp
 cd tmp
 git clone https://github.com/gpBlockchain/startBlockchain.git
 cd startBlockchain
-git checkout gp/support-sqlite 
 cd ckbDevWithIndexAndLightClient
 sh prepare.sh
 sh start.sh
